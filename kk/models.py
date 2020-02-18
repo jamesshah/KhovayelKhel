93ab8b16b63f5587154f9c8f5b8493bf059f0ec4
jamesshah <jdshah18072000@gmail.com>
2020-02-18 23:12:36 +0530

    changes

--- kk/models.py
+++ kk/models.py
@@ -3,7 +3,7 @@ from kk import db
 
 class User(db.Model):
 
-    __tablename__ = "user"
+    __tablename__ = "User"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
