1563c7fa003a6f537f542139ac829baea1fbfcef
jamesshah <jdshah18072000@gmail.com>
2020-02-18 22:48:20 +0530

    db changes

--- kk/models.py
+++ kk/models.py
@@ -3,6 +3,8 @@ from kk import db
 
 class User(db.Model):
 
+    __tablename__ = "user"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
