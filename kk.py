74010f9e567899eb0dc0df790f22408f91927e66
jamesshah <jdshah18072000@gmail.com>
2020-02-19 10:00:00 +0530

    fucking last time

--- kk.py
+++ kk.py
@@ -1,14 +1,7 @@
 from kk import app, dotenv_path, db
 import os
 from dotenv import load_dotenv
-from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
-
-migrate = Migrate(app, db)
-manager = Manager(app)
-
-manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
--- kk/__init__.py
+++ kk/__init__.py
@@ -10,13 +10,15 @@ APP_ROOT = os.path.join(os.path.dirname(__file__), '..')
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
 
+app.debug = os.getenv('DEBUG')
+app.port = os.getenv('PORT')
+
 app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config.update(
     MAIL_SERVER='smtp.gmail.com',
     MAIL_PORT=465,
     MAIL_USE_SSL=True,
-    MAIL_DEFAULT_SENDER=os.getenv('MAIL_USERNAME'),
     MAIL_USERNAME=os.getenv('MAIL_USERNAME'),
     MAIL_PASSWORD=os.getenv('MAIL_PASSWORD')
 )
